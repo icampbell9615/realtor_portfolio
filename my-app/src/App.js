9d614e160c07bd64fb6e5a570aaceab1301ad45f
Campbell,Ian <icampbell9615@gmail.com>
2020-04-15 18:11:40 -0400

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <React.Fragment>
+    <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+
+
+const Header = () => {
+    return (
+        <React.Fragment>
+      
+      <div className="hero-text">
+                    <h1 style={{fontSize:"350%"}}>Kevin Campbell</h1>
+                    <h3 style={{fontSize:"150%"}}>Charlotte's Finest Properties</h3>
+                </div>
+        </React.Fragment>
+
+    );
+};
+
+export default Header;
